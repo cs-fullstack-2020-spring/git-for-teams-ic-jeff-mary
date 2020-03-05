dd9a6b09b1dd3720f2f677c1d503454941adca94
JeffRThomas <thomas.jeff@live.com>
2020-03-05 11:22:24 -0600

    Upload base JS File

---
+++ jeff-mary.js
@@ -0,0 +1,15 @@
+console.log("Team JS Linked");
+
+// Get Reference variables for the three buttons
+let buttonOne = document.getElementById("");
+let buttoneTwo = document.getElementById("");
+let buttoneThree = document.getElementById("");
+// Debug Check to make sure the right elements were selected
+console.log(buttonOne);
+console.log(buttoneTwo);
+console.log(buttoneThree);
+
+buttonOne.addEventListener("click", changeBackground);
+changeBackground = (click) => {
+    
+}
